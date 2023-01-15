d4ac23634d7652794357524d970e3da26ae5624a
Louise Rosenqvist <93682181+lrosenqv@users.noreply.github.com>
2023-01-16 00:26:08 +0100

    spotify login prompt and auth

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { authRequest } from '@/services/authorization'
 defineProps<{
   msg: string
 }>()
@@ -12,6 +13,7 @@ defineProps<{
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
+    <button @click="authRequest">login</button>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import * as dotenv from 'dotenv'
-dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
