819ec516efc42a0b0ba02c23b5b7a36234cac1dc
Louise Rosenqvist <93682181+lrosenqv@users.noreply.github.com>
2023-01-15 17:29:35 +0100

    env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
