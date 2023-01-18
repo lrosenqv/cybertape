30bae94cbf0c4164bed25b0853bf078517334cd7
Louise Rosenqvist <93682181+lrosenqv@users.noreply.github.com>
2023-01-18 16:59:37 +0100

    removed unnecessary watch

--- src/views/DiscoverView.vue
+++ src/views/DiscoverView.vue
@@ -9,18 +9,14 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { getCategories } from '@/services/api'
 import type { ICategory } from '@/models/ICategory'
 
-const searchInput = ref('')
 const categories = ref<ICategory>({ href: '', items: [], next: '' })
 
 onMounted(async () => {
   categories.value = await getCategories()
-})
-watch(searchInput, (newVal) => {
-  console.log(newVal)
 })
 </script>
 
