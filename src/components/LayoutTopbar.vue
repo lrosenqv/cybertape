59ac8041e3769287f10dd796e29efce7f3283efe
Louise Rosenqvist <93682181+lrosenqv@users.noreply.github.com>
2023-02-06 23:47:56 +0100

    top nav user card

--- src/components/LayoutTopbar.vue
+++ src/components/LayoutTopbar.vue
@@ -16,13 +16,7 @@
           <RouterLink to="/discover" class="topbar-mobile-burger-nav-link">Discover</RouterLink>
           <RouterLink to="/mixer" class="topbar-mobile-burger-nav-link">Mixer</RouterLink>
         </nav>
-        <div class="topbar-mobile-user">
-          <img :src="store.state.currentUserImg" />
-          <div class="topbar-mobile-user__text">
-            <a :href="store.state.currentUserLink">{{ currentUser }}</a>
-            <button @click="logout">Log out</button>
-          </div>
-        </div>
+        <UserCard @logout="logout" />
       </template>
     </div>
 
@@ -31,15 +25,22 @@
         <RouterLink to="/discover" class="topbar-nav-link">Discover</RouterLink>
         <RouterLink to="/mixer" class="topbar-nav-link">Mixer</RouterLink>
       </nav>
-      <div class="topbar-divider"></div>
-      <div class="topbar-user">{{ currentUser }}</div>
+      <button class="topbar-user-btn" @click.stop="toggleUserCard"><IconUser /></button>
+      <UserCard
+        v-if="showUserCard"
+        class="topbar-user-card"
+        @close-card="toggleUserCard"
+        @logout="logout"
+      />
     </template>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref, toRefs, watch } from 'vue'
+import { computed, ref, toRefs } from 'vue'
 import { useStore } from 'vuex'
+import IconUser from '@/components/icons/IconUser.vue'
+import UserCard from '@/components/atoms/UserCard.vue'
 import { logout } from '@/services/authorization'
 const store = useStore()
 const currentUser = computed(() => store.state.currentUser)
@@ -48,12 +49,16 @@ const hideNav = ref<boolean>(false)
 
 const burgerMenu = ref<HTMLDivElement>()
 const menuOpen = ref<boolean>(false)
+const showUserCard = ref<boolean>(false)
 
 const app = document.body.querySelector('#app')
 
 function toggleBurger() {
   burgerMenu.value?.toggleAttribute('active')
   menuOpen.value = !menuOpen.value
+}
+function toggleUserCard() {
+  showUserCard.value = !showUserCard.value
 }
 const props = defineProps({
   mobileView: {
@@ -74,14 +79,6 @@ app?.addEventListener('scroll', () => {
     hideNav.value = false
   }
 })
-
-watch(
-  mobileView,
-  (newView) => {
-    console.log(newView)
-  },
-  { immediate: true }
-)
 </script>
 
 <style lang="scss" scoped>
@@ -128,29 +125,37 @@ watch(
     grid-column: 5 / 8;
     text-align: center;
     top: 0;
-    transition: all 0.8s;
+    transition: all 0.6s;
 
     &-link {
       width: 100%;
       text-align: center;
-      transition: all 0.8s;
+      transition: all 0.6s;
     }
 
     a:not(:last-of-type) {
       border-right: 2px solid variables.$color-neutral__greige-dark;
     }
   }
-
   &-user {
-    cursor: pointer;
-    grid-column: 9 / 13;
-    grid-row: 1;
-    line-height: 1;
-    margin-right: 12px;
-    text-align: end;
+    &__collapsed {
+      background-color: transparent;
+      width: fit-content;
+    }
+    &-btn {
+      background-color: transparent;
+      color: variables.$color-neutral__dark;
+      grid-column: 12;
+      height: 60px;
+      justify-self: flex-end;
+      width: 60px;
 
-    &:hover {
-      color: variables.$color__blue;
+      &:hover {
+        color: variables.$color__blue;
+      }
+    }
+    &-card {
+      grid-column: 11 / 13;
     }
   }
 
@@ -182,10 +187,8 @@ watch(
         border-color: transparent;
       }
     }
-    .topbar-user {
+    .topbar-user-btn {
       color: variables.$color-neutral__light;
-      grid-column: 12;
-      transition: all 1.2s;
     }
   }
 
@@ -197,8 +200,8 @@ watch(
       display: flex;
       flex-direction: column;
       justify-content: space-around;
-      height: 240px;
-      width: 180px;
+      height: 260px;
+      width: 250px;
       text-align: right;
       padding: 50px variables.$padding-large 0;
       position: absolute;
@@ -218,35 +221,9 @@ watch(
       display: flex;
       flex-direction: column;
       justify-content: flex-end;
+      gap: 15px;
       a {
         color: variables.$color-neutral__light;
-      }
-    }
-    &-user {
-      border-top: 2px solid variables.$color-neutral__greige-dark;
-      display: flex;
-      gap: 15px;
-      padding-top: variables.$padding-medium;
-      &__text {
-        display: flex;
-        flex-direction: column;
-        text-align: left;
-        a {
-          color: variables.$color-neutral__light;
-          font-weight: 600;
-        }
-        button {
-          background-color: transparent;
-          color: variables.$color__yellow;
-          @include variables.font-size-paragraph;
-          &:hover {
-            text-decoration: underline;
-          }
-        }
-      }
-      img {
-        border-radius: 50%;
-        width: 50px;
       }
     }
 
